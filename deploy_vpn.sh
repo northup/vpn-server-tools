f5f9a38d84dae42331f952bfa3bfd66cd69cf06a
Northup <northup.live@gmail.com>
2013-06-04 01:39:32 +0800

    Update deploy_vpn.sh
    fix sysctl

--- deploy_vpn.sh
+++ deploy_vpn.sh
@@ -87,7 +87,7 @@ apt-get update
 apt-get upgrade -y
 
 # sysctl.conf
-sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/g' /etc/sysctl.conf
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 sysctl -p
 
 # IPSec（StrongSwan）
